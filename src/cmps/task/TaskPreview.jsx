bb3160d598c3c1f07c921aeac062f6ffe5aea7af
oxanashva <oxanashav@gmail.com>
2025-10-21 03:55:18 +0300

    feat: add task badges TaskPreview component

--- src/cmps/task/TaskPreview.jsx
+++ src/cmps/task/TaskPreview.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from 'react'
 import { useParams } from "react-router"
+import { Link } from 'react-router'
 
 import CircleIcon from '../../assets/images/icons/circle.svg?react'
 import CircleCheckIcon from '../../assets/images/icons/circle-check.svg?react'
 import EditIcon from '../../assets/images/icons/edit.svg?react'
-import { Link } from 'react-router'
+import DescriptionIcon from '../../assets/images/icons/description.svg?react'
+import CommentIcon from '../../assets/images/icons/comment.svg?react'
 
-export function TaskPreview({ task, onCompleteTask }) {
+export function TaskPreview({ board, task, onCompleteTask }) {
     const { boardId } = useParams()
     const [isChecked, setIsChecked] = useState(task.closed || false)
+
+    const comments = board?.actions.filter(action => {
+        return action.data.idTask === task._id
+    })
 
     useEffect(() => {
         setIsChecked(task.closed || false)
@@ -34,6 +40,20 @@ export function TaskPreview({ task, onCompleteTask }) {
                                 : <span title="Mark complete"><CircleIcon width={16} height={16} fill="currentColor" /></span>}
                         </div>
                         <span>{task.name}</span>
+                    </div>
+                    {/* TODO: update task.badges on add comment/description or another action and use them to conditionally render */}
+                    <div className="task-badges">
+                        {task.desc &&
+                            <span>
+                                <DescriptionIcon width={16} height={16} fill="currentColor" />
+                            </span>
+                        }
+                        {comments.length !== 0 &&
+                            <span className="badge">
+                                <CommentIcon width={16} height={16} fill="currentColor" />
+                                <span>{comments.length}</span>
+                            </span>
+                        }
                     </div>
                     <button className="edit-btn">
                         <EditIcon width={16} height={16} fill="currentColor" />
