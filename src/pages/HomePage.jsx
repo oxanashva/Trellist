91603c08ad047b605dfcdb5819940d98f7b1ef67
oxanashva <oxanashav@gmail.com>
2026-03-18 16:24:58 +0200

    fix: update application name across UI and metadata

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ export function HomePage() {
         <section className="home">
             <div className="hero-content">
                 <div className="hero-text">
-                    <h1>Trellist brings all your tasks, teammates, and tools together</h1>
+                    <h1>Trellis brings all your tasks, teammates, and tools together</h1>
                     <p>Keep everything in the same place-even if your team isn't.</p>
                     <NavLink to="/workspace" className="link-btn btn-primary">
                         Start Demo
